19f3e293ffe6174571ee12af64b0dd003fc0d8ce
Tomoyuki Shimizu <smz2develop@gmail.com>
2020-02-06 09:41:50 +0900

    Make About page

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <section class="has-text-centered section">
+      <p>作成中</p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>

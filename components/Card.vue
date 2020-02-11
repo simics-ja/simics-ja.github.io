5bcaec5bf19461edf17c1ea7a41552fdd67cb6e6
Tomoyuki Shimizu <smz2develop@gmail.com>
2020-02-12 08:20:12 +0900

    Make job-hunt page

---
+++ components/Card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image">
+        <img :src="image">
+      </figure>
+    </div>
+    <div class="media-content">
+      <p class="title">
+        {{ title }}
+      </p>
+    </div>
+    <div class="content">
+      {{ content }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    content: String,
+    image: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  color: black;
+}
+
+.content {
+  color: black;
+}
+</style>
---
+++ pages/job-hunt.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <section class="has-text-centered section">
+      <p v-for="(prj, i) in projects" :key="i">
+        <card :title="prj.title" :content="prj.content" :image="prj.image" />
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+import card from '~/components/Card'
+
+export default {
+  components: {
+    card
+  },
+  data () {
+    return {
+      projects: [
+        { title: 'test-title', content: 'test-content', image: 'https://www.google.co.jp/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style lang="scss">
+</style>

dffe28d70bb86f32eb2050ab7efe9a29fbd5e161
Tomoyuki Shimizu <smz2develop@gmail.com>
2020-02-18 11:59:08 +0900

    Update about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="container">
-    <section class="has-text-centered section">
-      <p>作成中</p>
+  <div class="container has-text-centered">
+    <section class="section is-mx-auto">
+      <h2 class="is-size-5 is-mb-3">
+        清水 友順（しみず ともゆき）
+      </h2>
+      <p class="has-text-justified">
+        1993年6月17日広島県生まれ．神戸大学大学院 工学研究科博士課程後期課程在学．研究分野はヒューマンコンピュータインタラクション（HCI）であり，情報が人の心身や意思決定に与える影響の調査やその影響に基づいたインタフェースデザイン等の応用について研究．
+      </p>
     </section>
   </div>
 </template>
@@ -16,4 +21,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+section {
+  max-width: 800px;
+}
 </style>

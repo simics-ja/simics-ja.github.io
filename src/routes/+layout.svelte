df9c3a0fc136ed4e6c597f1263eed31dead17b50
Tomoyuki Shimizu <smz2develop@gmail.com>
2023-01-05 00:08:29 +0900

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '$lib/styles/global.scss';
+	export const prerender = true;
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/blog">About</a>
+</nav>
+
+<slot />
+
+<style lang="scss">
+</style>
